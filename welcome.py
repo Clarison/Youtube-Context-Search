af241a7a26c4d23ff1bd78792997828f7d0c90a0
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-11 18:16:54 -0400

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,4 +1,14 @@
 import streamlit as st
+from urllib.parse import urlparse, parse_qs
 st.title("YouTube Transcript Embeddings Search")
 
-video_id = st.text_input("Enter a YouTube video ID:")
+video_id = st.text_input("Enter a YouTube video url:")
+
+# get the video ID and channel from the URL
+url_components = urlparse(video_id)
+query_params = parse_qs(url_components.query)
+video_id = query_params["v"][0]
+channel = query_params["ab_channel"][0]
+
+st.write(channel)
+
