36c42f04031bd679af794faa0d3b0e61a3786f33
Clarison Dsilva <98853545+Clarison@users.noreply.github.com>
2023-04-12 22:25:32 -0400

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -16,4 +16,17 @@ channel = query_params["ab_channel"][0]
 st.write(channel)
 st.write(video_id)
 
+res = openai.Completion.create(
+    engine='text-davinci-003',
+    prompt=query,
+    temperature=0,
+    max_tokens=400,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None
+)
+
+res['choices'][0]['text'].strip()
+
 
